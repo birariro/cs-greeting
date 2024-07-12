51a93dbaa6a972dcc73763aadf3755d76ce6cc22
birariro <vps32@naver.com>
2024-07-12 11:44:33 +0900

    문제 정리

--- greeting.sh
+++ greeting.sh
@@ -1,26 +1,34 @@
 #!/bin/sh
 
-JSON_FILE="$(dirname $0)/db.json"
+## random 'question type' select
+JSON_FILES=("$(dirname $0)"/db/*.json)
+JSON_FILES_SIZE=${#JSON_FILES[@]}
+RANDOM_INDEX=$(( RANDOM % JSON_FILES_SIZE ))
+JSON_FILE="${JSON_FILES[$RANDOM_INDEX]}"
 
+## 'question type' print
+FILE_NAME=$(basename "$JSON_FILE")
+echo "[ ${FILE_NAME%.json} ]"
+
+## random 'question type' in question select
 LENGTH=$(jq length "$JSON_FILE")
-
 RANDOM_INDEX=$((RANDOM % LENGTH))
-
 RANDOM_OBJECT=$(jq -r ".[$RANDOM_INDEX]" "$JSON_FILE")
 
+## question parsing
 KEY=$(echo "$RANDOM_OBJECT" | jq -r 'keys_unsorted[0]')
 VALUE=$(echo "$RANDOM_OBJECT" | jq -r ".\"$KEY\"")
 
-
-
-# 맞침표(.) 뒤에 개행 문자 추가
-if [[ "$VALUE" == *"."* ]]; then
-    output=$(echo "$VALUE" | sed 's/\./.\n/')
-else
+# 맞침표(.) 뒤에 개행 문자
+if [[ "$VALUE" == *".  "* ]]; then
+    output=$(echo "$VALUE" | sed 's/\.  /.\n/g')
+elif [[ "$VALUE" == *". "* ]]; then
+    output=$(echo "$VALUE" | sed 's/\. /.\n/g')
+else 
     output="$VALUE"
 fi
 
-# 결과 출력
 echo "[ $KEY ]"
+echo ""
 echo "$output"
 echo ""
